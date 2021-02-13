acaaf82109ea73c1e8b9cbef61209a63d56fbdb0
muhammadminhas <78682556+muhammadminhas@users.noreply.github.com>
2021-02-13 20:38:41 +0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+import Home from './Components/Home';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Route exact path="/" component={Home}/>
+     
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import AuthContext from './Context/AuthContext';
+
+ReactDOM.render(<AuthContext><App/>,</AuthContext>,document.getElementById('root'));
